6bcee6f75dd3b53a114314de22545b07adcb76f6
Gil <gilvicentevieira@gmail.com>
2024-01-14 18:46:21 +0000

    Addind configuration for images. Adding image component to single drink page

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
@@ -14,11 +15,14 @@ const SingleDrinkPage = async ({params}) => {
     const data = await getSingleDrink(params.id)
     const title = data?.drinks[0]?.strDrink
     const src = data?.drinks[0]?.strDrinkThumb
+
+    console.log(src)
     return (
         <div>
             <Link href='/drinks' className="btn btn-primary mt-8 mb-12">
                 back to drinks
             </Link>
+            <Image src={src} width={300} height={300} className="w-48 h-48 rounded-lg shadow-lg mb-4" alt={title}/>
             <h1 className="text-4xl mb-8">{title}</h1>
         </div>
     )
