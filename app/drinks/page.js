b34025e5d501d5d8dfbba2f36be74277f1f703c1
Gil <gilvicentevieira@gmail.com>
2024-01-14 16:53:44 +0000

    Added loading ui to drinks page

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,11 +1,14 @@
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 
-const DrinksPage = async () => {
+const fetchDrinks = async ()=>{
+  await new Promise((resolve)=> setTimeout(resolve, 1000))
   const response = await fetch(url)
   const data = await response.json()
+  return data
+}
 
-  console.log(data)
-
+const DrinksPage = async () => {
+  const data = await fetchDrinks()
   return (
     <div>
         <h1 className="text-7xl">
