085f5f9e58e0e8280dba15bb826d5085c9d39b42
Gil <gilvicentevieira@gmail.com>
2024-01-14 18:25:41 +0000

    Added single drink page logic

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,7 +1,26 @@
-const SingleDrinkPage = ({params}) => {
-    console.log(params)
+import Link from "next/link"
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+
+const getSingleDrink = async (id)=>{
+    const response = await fetch(url + id)
+    if(!response.ok){
+        throw new Error('Failed to fetch a single drink')
+    }
+    return response.json()
+}
+
+const SingleDrinkPage = async ({params}) => {
+    const data = await getSingleDrink(params.id)
+    const title = data?.drinks[0]?.strDrink
+    const src = data?.drinks[0]?.strDrinkThumb
     return (
-        <div>SingleDrinkPage</div>
+        <div>
+            <Link href='/drinks' className="btn btn-primary mt-8 mb-12">
+                back to drinks
+            </Link>
+            <h1 className="text-4xl mb-8">{title}</h1>
+        </div>
     )
 }
 
