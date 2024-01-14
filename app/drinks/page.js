bc6fff62342fd62d86bd48ffa0869bfe45a2d1fc
Gil <gilvicentevieira@gmail.com>
2024-01-14 15:35:12 +0000

    Adding pages: client, drinks, prisma-example, query and tasks

---
+++ app/drinks/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DrinksPage = () => {
+  return (
+    <div>
+        <h1 className="text-7xl">
+            DrinksPage
+        </h1>
+    </div>
+  )
+}
+
+export default DrinksPage
