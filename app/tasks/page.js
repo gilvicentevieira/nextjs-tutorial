5418b8453ddec104df4d3d095f7c9835c8ce7e75
Gil <gilvicentevieira@gmail.com>
2024-01-15 02:09:36 +0000

    Added force dynamic to tasks page

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -2,6 +2,8 @@ import TaskFormCustom from '@/components/TaskFormCustom'
 import TaskList from '@/components/TaskList'
 import React from 'react'
 
+export const dynamic = 'force-dynamic'
+
 const TasksPage = () => {
   return (
     <div className='max-w-lg'>
