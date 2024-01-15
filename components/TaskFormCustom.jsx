4ec5a04934e56981107d6b7d27d101ca04645f46
Gil <gilvicentevieira@gmail.com>
2024-01-15 00:59:34 +0000

    Added toast messages to taskFormCustom

--- components/TaskFormCustom.jsx
+++ components/TaskFormCustom.jsx
@@ -1,6 +1,8 @@
 'use client'
 import { createTaskCustom } from "@/utils/actions"
+import { useEffect } from "react"
 import {useFormStatus, useFormState} from 'react-dom'
+import toast from "react-hot-toast"
 
 const SubmitBtn = ()=>{
   const {pending} = useFormStatus()
@@ -17,6 +19,18 @@ const initialState = {
 
 const TaskFormCustom = () => {
   const [state, formAction] = useFormState(createTaskCustom, initialState)
+
+  useEffect(()=>{
+    if(state.message === 'error'){
+      toast.error('there was an error')
+      return 
+    }
+
+    if(state.message){
+      toast.success('task created')
+    }
+  }, [state])
+
   return (
     <form action={formAction}>
       {state.message ? <p className="mb-2">{state.message}</p> : null}
